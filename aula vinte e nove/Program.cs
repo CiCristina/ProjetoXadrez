5a347291050488ae68966271d1e1c6854ad526fa
Cibelly Cristina <11637542690@est.udicentro.local>
2016-09-26 09:22:39 -0300

    Primeiro commit

---
+++ aula vinte e nove/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace aula_vinte_e_nove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite um valor inteiro: ");
+
+            int x = int.Parse(Console.ReadLine());
+
+            int y = 2 * x;
+
+
+            Console.WriteLine("DOBRO = " + y);
+
+            Console.ReadLine();
+         
+        }
+    }
+}
