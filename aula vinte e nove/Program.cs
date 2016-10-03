a0b1957ea72908c34a30a4673bd28a66c9374e5a
Cibelly Cristina <jrodriguescorreios@hotmail.com>
2016-10-03 11:06:21 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- aula vinte e nove/Program.cs
+++ aula vinte e nove/Program.cs
@@ -10,12 +10,24 @@ namespace aula_vinte_e_nove
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            PosicaoXadez pos = new PosicaoXadez('c', 7);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            Console.WriteLine(pos);
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
-            Console.WriteLine(pos.toPosicao());
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
 
             Console.ReadLine();
         }
